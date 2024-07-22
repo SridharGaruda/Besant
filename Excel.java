e648f049691b551945087a9352663d079cef01fe
SridharGaruda <sridhar.auminfo@gmail.com>
2024-07-22 17:39:30 +0530

    updating the main method

--- Excel.java
+++ Excel.java
@@ -1,4 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Excel {
 
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demoqa.com/droppable");
+		driver.manage().window().maximize();
+		
+		WebElement src = driver.findElement(By.id("draggable"));
+		
+		WebElement destiny = driver.findElement(By.id("droppable"));
+		
+		Actions act = new Actions(driver);
+		act.clickAndHold(src)
+		.moveToElement(destiny)
+		.release()
+		.build()
+		.perform();
+		
+		Thread.sleep(2000);
+		driver.quit();
+		
+	
+}
 }
